aab7894e932bebd7d3a22513e89ec604c8b6bfce
Luis Flores <100862385+LuisFLores7247@users.noreply.github.com>
2023-05-10 20:38:44 -0600

    Modificamos los menu

--- proyectoFinal.cpp
+++ proyectoFinal.cpp
@@ -18,10 +18,8 @@ void portada(int consoleWidth);      /* Caso 2 */
 void presentacion(int consoleWidth); /* Caso 2 */
 
 // Menus
-int menu(int consoleWidth);       /* Caso 4 */
-void menuJuego(int consoleWidth); /* Caso 2 */
-void mantenimiento();             /* Caso 1 */
 int menu(int consoleWidth);                             /* Caso 4 */
+void menuJuego(int consoleWidth);                       /* Caso 2 */
 void mantenimiento();                                   /* Caso 1 */
 void opcionesMantenimiento(int arch, int consoleWidth); /* Caso 2 */
 
@@ -29,9 +27,15 @@ void opcionesMantenimiento(int arch, int consoleWidth); /* Caso 2 */
 int **pedirMemoria(int ren, int col); /* Caso 4 */
 void gotoxy(int x, int y);            /* Caso 2 */
 void quitarYponerDiag(char[], bool);  /* Caso 2 */
-bool validarAlias(string alias);      /* Caso 2 */
-bool validarCategoria(string cat);    /* Caso 2 */
-bool validarPalabras(int words);      /* Caso 2 */
+
+// Assets
+bool validarAlias(string alias);   /* Caso 2 */
+bool validarCategoria(string cat); /* Caso 2 */
+bool validarPalabras(int words);   /* Caso 2 */
+
+// Archivos
+void leerArch(int arch, int consoleWidth);
+void modificar(int arch, int consoleWidth);
 
 int main(int argc, char const *argv[])
 {
@@ -45,6 +49,7 @@ int main(int argc, char const *argv[])
     do
     {
         opc = menu(consoleWidth);
+
         switch (opc)
         {
         case 1:
@@ -64,10 +69,9 @@ int main(int argc, char const *argv[])
 
         default:
             cout << "Opcion no valida, por favor intente de nuevo. " << endl;
-            system("pause");
             break;
         }
-    } while (opc < 1 || opc > 5);
+    } while (opc != 5 || (opc < 1 || opc > 5));
 
     return 0;
 }
@@ -181,12 +185,12 @@ int menu(int consoleWidth)
 
 void mantenimiento()
 {
-    system("cls");
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
     GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
     int consoleWidth = consoleInfo.dwSize.X;
     int opc;
+    system("cls");
     string line = "Categoria a modificar";
     int leftPadding = (consoleWidth - line.length()) / 2;
     gotoxy(leftPadding, 1);
@@ -199,7 +203,7 @@ void mantenimiento()
     leftPadding = (consoleWidth - line.length()) / 2;
     gotoxy(leftPadding, 5);
     cout << line;
-    line = "3.- Categoria 3";
+    line = "3.- Paises";
     leftPadding = (consoleWidth - line.length()) / 2;
     gotoxy(leftPadding, 7);
     cout << line;
@@ -220,8 +224,12 @@ void mantenimiento()
     case 3:
         opcionesMantenimiento(3, consoleWidth);
         break;
-
     default:
+        line = "Opcion incorrecta";
+        leftPadding = (consoleWidth - line.length()) / 2;
+        gotoxy(leftPadding, 13);
+        cout << line;
+
         break;
     }
 }
@@ -251,6 +259,15 @@ void opcionesMantenimiento(int arch, int consoleWidth)
     gotoxy(leftPadding, 13);
     cout << line;
     cin >> opc;
+    switch (opc)
+    {
+    case 1:
+        leerArch(arch, consoleWidth);
+        break;
+
+    default:
+        break;
+    }
 }
 
 void menuJuego(int consoleWidth)
@@ -275,15 +292,15 @@ void menuJuego(int consoleWidth)
         leftPadding = (consoleWidth - line.length()) / 2;
         gotoxy(leftPadding, 5);
         cout << line;
-        line = "Dispositivos electronicos";
+        line = "1.- Dispositivos electronicos";
         leftPadding = (consoleWidth - line.length()) / 2;
         gotoxy(leftPadding, 7);
         cout << line;
-        line = "Lenguajes de programacion";
+        line = "2.- Lenguajes de programacion";
         leftPadding = (consoleWidth - line.length()) / 2;
         gotoxy(leftPadding, 8);
         cout << line;
-        line = "Paises";
+        line = "3.- Paises";
         leftPadding = (consoleWidth - line.length()) / 2;
         gotoxy(leftPadding, 9);
         cout << line;
@@ -293,7 +310,7 @@ void menuJuego(int consoleWidth)
         cout << line;
         fflush(stdin);
         getline(cin, catAJugar);
-        //Validar cat
+        // Validar cat
         line = "Con cuantas Palabras deseas Jugar (3,6 u 8 palabras): ";
         leftPadding = (consoleWidth - line.length()) / 2;
         gotoxy(leftPadding, 12);
@@ -303,7 +320,7 @@ void menuJuego(int consoleWidth)
         gotoxy(leftPadding, 13);
         cout << line;
         cin >> palAUsar;
-        //validar palabras
+        // validar palabras
     } while (!validarAlias(alias) || !validarCategoria(catAJugar) || !validarPalabras(palAUsar));
 }
 
@@ -329,84 +346,186 @@ void gotoxy(int x, int y)
     SetConsoleCursorPosition(hcon, dwPos);
 }
 
-
-//Assets 
+// Assets
 
 bool validarAlias(string alias)
 {
-    bool aux=true;
-    //condiciones de validacion
-    if (alias.length()<4 || alias.length()>12)
+    bool aux = true;
+    // condiciones de validacion
+    if (alias.length() < 4 || alias.length() > 12)
     {
-        aux=false;
-        cout<<"La longitud del Alias no es permitida, por favor inserte un alias con mas de 4 caracteres y menos de 12"<<endl;
+        aux = false;
+        cout << "La longitud del Alias no es permitida, por favor inserte un alias con mas de 4 caracteres y menos de 12" << endl;
     }
     if (!isalpha(alias[0]))
     {
-        aux=false;
-        cout<<"El primer caracter del alias debe de ser una letra, intentelo de nuevo"<<endl;
-
+        aux = false;
+        cout << "El primer caracter del alias debe de ser una letra, intentelo de nuevo" << endl;
     }
-    for (int i = 1; i < alias.size(); i++)  //desde i=1 pk i=0 ya se valido
+    for (int i = 1; i < alias.size(); i++) // desde i=1 pk i=0 ya se valido
     {
         if (isspace(alias[i]))
         {
-            aux=false;
-            cout<<"Caracter invalido en el alias, solo se permiten letras, numeros y guiones bajos"<<endl;
+            aux = false;
+            cout << "Caracter invalido en el alias, solo se permiten letras, numeros y guiones bajos" << endl;
         }
-        
-    }    
+    }
     if (!aux)
     {
-        cout<<"El alias Ingresado no es correcto, por favor intente de nuevo"<<endl;
+        cout << "El alias Ingresado no es correcto, por favor intente de nuevo" << endl;
         system("pause");
     }
     return aux;
 }
 bool validarCategoria(string cat)
 {
-    bool aux=false;
-    //condiciones de validacion
-    if (cat=="Dispositivos electronicos")
+    bool aux = false;
+    // condiciones de validacion
+    if (cat == "Dispositivos electronicos")
     {
-        aux=true;
+        aux = true;
     }
-    if (cat=="Lenguajes de programacion")
+    if (cat == "Lenguajes de programacion")
     {
-        aux=true;
+        aux = true;
     }
-    if (cat=="Paises")
+    if (cat == "Paises")
     {
-        aux=true;
-    }    
+        aux = true;
+    }
     if (!aux)
     {
-        cout<<"La categoria ingresada no es correcta, por favor intente de nuevo"<<endl;
+        cout << "La categoria ingresada no es correcta, por favor intente de nuevo" << endl;
         system("pause");
     }
     return aux;
 }
 bool validarPalabras(int words)
 {
-    bool aux=false;
-    //condiciones de validacion
-    if (words==3)
+    bool aux = false;
+    // condiciones de validacion
+    if (words == 3)
     {
-        aux=true;
+        aux = true;
     }
-    if (words==6)
+    if (words == 6)
     {
-        aux=true;
+        aux = true;
     }
-    if (words==8)
+    if (words == 8)
     {
-        aux=true;
+        aux = true;
     }
-    
+
     if (!aux)
     {
-        cout<<"La opcion de palabras ingresada no es correcta, por favor intente de nuevo"<<endl;
+        cout << "La opcion de palabras ingresada no es correcta, por favor intente de nuevo" << endl;
         system("pause");
     }
     return aux;
+}
+
+// Archivos
+
+void leerArch(int arch, int consoleWidth)
+{
+    fstream file;
+    string line;
+    int leftPadding;
+    system("cls");
+    if (arch == 1)
+    {
+        file.open(CAT1, ios::in);
+        line = "DISPOSITIVOS MOVILES";
+        int leftPadding = (consoleWidth - line.length()) / 2;
+        gotoxy(leftPadding, 1);
+        cout << line;
+    }
+    if (arch == 2)
+    {
+        file.open(CAT2, ios::in);
+        line = "LENGUAJES DE PROGRAMACION";
+        leftPadding = (consoleWidth - line.length()) / 2;
+        gotoxy(leftPadding, 1);
+        cout << line;
+    }
+    if (arch == 3)
+    {
+        file.open(CAT3, ios::in);
+        line = "PAISES";
+        leftPadding = (consoleWidth - line.length()) / 2;
+        gotoxy(leftPadding, 1);
+        cout << line;
+    }
+    int i = 3;
+    while (getline(file, line))
+    {
+        leftPadding = (consoleWidth - line.length()) / 2;
+        gotoxy(leftPadding, i);
+        for (int i = 0; i < line.length(); i++)
+        {
+            if (line[i] == '_')
+            {
+                cout << " ";
+            }
+            else
+            {
+                cout << line[i];
+            }
+        }
+
+        i += 2;
+    }
+    leftPadding = (consoleWidth - 32) / 2;
+    gotoxy(leftPadding, i);
+    system("pause");
+}
+
+void modificar(int arch, int consoleWidth)
+{
+    fstream temp, file;
+    string line;
+    int leftPadding;
+
+    if (arch == 1)
+    {
+        file.open(CAT1, ios::in);
+        line = "DISPOSITIVOS MOVILES";
+        leftPadding = (consoleWidth - line.length()) / 2;
+        gotoxy(leftPadding, 1);
+        cout << line;
+    }
+    if (arch == 2)
+    {
+        file.open(CAT2, ios::in);
+        line = "LENGUAJES DE PROGRAMACION";
+        leftPadding = (consoleWidth - line.length()) / 2;
+        gotoxy(leftPadding, 1);
+        cout << line;
+    }
+    if (arch == 3)
+    {
+        file.open(CAT3, ios::in);
+        line = "PAISES";
+        leftPadding = (consoleWidth - line.length()) / 2;
+        gotoxy(leftPadding, 1);
+        cout << line;
+    }
+    temp.open("temp.txt", ios::out);
+
+    string palabra;
+    int id, i = 1;
+    cout << "Ingrese el ID a modificar: ";
+    cin >> id;
+
+    while (file >> palabra)
+    {
+        if (id == i)
+        {
+                }
+    }
+    temp.close();
+    file.close();
+    system("del datos.txt");
+    system("ren temp.txt datos.txt");
 }
